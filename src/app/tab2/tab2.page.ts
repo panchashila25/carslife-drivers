31a036f23723dcc9b9bf6bd615e3f477e05f9916
krushnadeshmukh <krishnadeshmukh176@gmail.com>
2023-11-08 18:11:05 +0530

    changes

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,17 @@ import { ApiService } from '../core/api.service';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  features: any[] = [
+    {id: 1, name: 'Add', src: 'assets/img/add-money.jpg', background: 'rgba(27,150,181, 0.1)', page: ''},
+    {id: 2, name: 'Withdraw', src: 'assets/img/withdraw.jpg', background: 'rgba(106,100,255, 0.1)', page: ''},
+    {id: 3, name: 'Send', src: 'assets/img/send.jpg', background: 'rgba(255, 196, 9, 0.1)', page: ''},
+    {id: 4, name: 'Pay', src: 'assets/img/pay.jpg', background: 'rgba(27,150,181, 0.1)', page: ''},
+  ];
+
+  transactions: any[] = [
+    {id: 1, vendor: 'Added to wallet', image: '', amount: 1500, time: '3:00PM'},
+    {id: 2, vendor: 'Debited from wallet', image: '', amount: -1200, time: '4:00PM'}
+  ];
 
   constructor(public api:ApiService) {}
    createTransaction(){
