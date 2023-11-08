e10c12fcdef34cc0dbf362eeac43beea2736249d
panchashila25 <panchashila.ranbagle@tectiq.in>
2023-11-08 19:58:17 +0530

    changes

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../core/api.service';
 
 @Component({
@@ -6,21 +6,26 @@ import { ApiService } from '../core/api.service';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
-  features: any[] = [
-    {id: 1, name: 'Add', src: 'assets/img/add-money.jpg', background: 'rgba(27,150,181, 0.1)', page: ''},
-    {id: 2, name: 'Withdraw', src: 'assets/img/withdraw.jpg', background: 'rgba(106,100,255, 0.1)', page: ''},
-    {id: 3, name: 'Send', src: 'assets/img/send.jpg', background: 'rgba(255, 196, 9, 0.1)', page: ''},
-    {id: 4, name: 'Pay', src: 'assets/img/pay.jpg', background: 'rgba(27,150,181, 0.1)', page: ''},
-  ];
+export class Tab2Page  implements OnInit{
+  // features: any[] = [
+  //   {id: 1, name: 'Add', src: 'assets/img/add-money.jpg', background: 'rgba(27,150,181, 0.1)', page: ''},
+  //   {id: 2, name: 'Withdraw', src: 'assets/img/withdraw.jpg', background: 'rgba(106,100,255, 0.1)', page: ''},
+  //   {id: 3, name: 'Send', src: 'assets/img/send.jpg', background: 'rgba(255, 196, 9, 0.1)', page: ''},
+  //   {id: 4, name: 'Pay', src: 'assets/img/pay.jpg', background: 'rgba(27,150,181, 0.1)', page: ''},
+  // ];
 
   transactions: any[] = [
     {id: 1, vendor: 'Added to wallet', image: '', amount: 1500, time: '3:00PM'},
     {id: 2, vendor: 'Debited from wallet', image: '', amount: -1200, time: '4:00PM'}
   ];
 
-  constructor(public api:ApiService) {}
+  constructor(public api:ApiService)  {}
+  ngOnInit(): void {
+    this.createTransaction()
+  }
    createTransaction(){
-    this.api.createTransaction
+  this.api.getTransaction({}).subscribe((cdata:any)=>{
+      console.log(cdata.user)
+    })
    }
 }
